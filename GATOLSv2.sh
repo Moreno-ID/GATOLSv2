ac5d31d36bcb0aae7ca0cf8fff096d41f490afb8
3RR0R <50474363+Moreno-ID@users.noreply.github.com>
2019-05-25 07:56:45 +0000

    Add files via upload

---
+++ GATOLSv2.sh
@@ -0,0 +1,827 @@
+#!/bin/bash
+#RecodeGakAkanBuatLuPro
+#SavePalestina
+
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pu='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+clear
+echo "卐卐卐卐卐卐        卐
+          卐        卐
+          卐        卐
+          卐        卐
+          卐        卐
+卐卐卐卐卐卐卐卐卐卐卐
+卐        卐
+卐        卐
+卐        卐
+卐        卐
+卐        卐卐卐卐卐卐" |lolcat -a -d 10
+figlet -f slant "GATOLSv2" |lolcat -a -d 10
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" |lolcat -a -d 10
+echo "Name    : 3RR0R" |lolcat -a -d 10
+echo "Github  : Moreno-ID" |lolcat -a -d 10
+echo "Wa      : 085332814651" |lolcat -a -d 10
+echo "Fb      : Moreno ID" |lolcat -a -d 10
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" |lolcat -a -d 10
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $me"[+]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo $cy"[+]> Thanks"
+sleep 1
+echo ""
+echo $pu"[+]> see you gaes :)..."
+sleep 1
+exit
+}
+
+echo $me"1.REPORT FB"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ku"2.BRUTE PORCE"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"3.SPAM SMS"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"4.PHISING"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"5.DEFACE"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"6.WebDav"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"7.Foto Profile GUARD FB"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $me"8.Hack-Wifi"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ku"9.Script-Deface"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"10.5Tools"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"11.OSIF"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"12.Yahoo Clone"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"13.Hack Fb Massal[MBF]"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"14.Lazymux"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $me"15.Nmap"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ku"16.TextToBinary"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"17.Tools Mr.Cakil"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"18.LazyGo-v1"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"19.ACN"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"20.IslamPray"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"21.KALSEL-Z-TOOL"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $me"22.Hack-CCTV"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ku"23.DDos"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"24.TOOLSINSTALLERv1"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"25.TOOLAINSTALLERv2"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"26.TOOLSINSTALLERv3"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"27.TOOLSINSTALLERv4"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"28.Perkiraan Cuaca"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"29.ShortLink"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"30.Dorking"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"31.Admin Finder"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"32.Admin Scan"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"33.A-Rat Exploit"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"34.Weeman"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"35.SocialFish"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"36.Red Hawk"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"37.D-TECT"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"38.IPGeolocation"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"39.Matrix"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"40.Termux-ohmyzsh"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"41.Password Generator"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"42.Lazy Figlet"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"43.MapSCII"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"44.LazyScript"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"45.Animasi Kereta Api :v"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"46.Spam Email"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"47.Informasi Sistem HP"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"48.Kode Unik FB Lite"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"49.Adzan&Al-Quran"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"50.Bug Report"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"51.Dengar Or Download Lagu"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"52.Auto React FB"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"53.Login Termux"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"54.Munculin Button Kiri&Kanan"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"55.Perbandingan Suhu"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"56.Chat Via Termux"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"57.FakeRoot"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"58.Moon Buggy"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"59.SQLMap"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"60.visSQL Scan Vuln"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"99.Install Bahan"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"00.Exit"
+echo "╭─(pilih)" |lolcat
+read -p"╰─#" kaex
+
+if [ $kaex = 1 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $kaex = 2 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/fbbrute
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $kaex = 3 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/SPAM
+cd SPAM
+sh SPAM.sh
+fi
+
+if [ $kaex = 4 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/PHISING
+cd PHISING
+sh Phis.sh
+fi
+
+if [ $kaex = 5 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/DEFACE
+cd DEFACE
+sh DFC.sh
+fi
+
+if [ $kaex = 6 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/WebDav
+cd WebDav
+sh WebDav.sh
+fi
+
+if [ $kaex = 7 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/kumpulanremaja/pp-fb
+cd pp-fb
+php fb.php
+fi
+
+if [ $kaex = 8 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/Hack-Wifi
+cd Hack-Wifi
+sh Wifi.sh
+fi
+
+if [ $kaex = 9 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/Script-Deface
+cd Script-Deface
+python2 script-deface.py
+fi
+
+if [ $kaex = 10 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/5TOOLS
+cd 5TOOLS
+sh 5tools.sh
+fi
+
+if [ $kaex = 11 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF
+cd OSIF
+python2 osif.py
+fi
+
+if [ $kaex = 12 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+python2 cloning.py
+fi
+
+if [ $kaex = 13 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/pirmansx/MBF
+cd MBF
+python2 MBF.py
+fi
+
+if [ $kaex = 14 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+https://github.com/Gameye98/Lazymux
+cd Lazymux
+python lazymux.py
+fi
+
+if [ $kaex = 15 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+pkg install nmap
+echo -e  "${y} {1} Masukkan Web${endc}:"
+read web
+nmap $web
+echo
+fi
+
+if [ $kaex = 16 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/FajarTheGGman/TextToBinary
+mv TextToBinary $HOME
+cd $HOME/TextToBinary
+sh install
+fi
+
+if [ $kaex = 17 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/mrcakil/Mrcakil
+cd Mrcakil
+./tools
+fi
+
+if [ $kaex = 18 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/kadal15/LazyGo-v1
+cd LazyGo-v1
+sh lazygo
+fi
+
+if [ $kaex = 19 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/MaulanaRyM/ACN
+cd ACN
+sh ACN.sh
+fi
+
+if [ $kaex = 20 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/FajarTheGGman/IslamPray
+cd IslamPray
+php islam.php
+fi
+
+if [ $kaex = 21 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/KALSELeXploit/KALSEL-Z-TOOL
+cd KALSEL-Z-TOOL
+sh Z-TOOL.sh
+fi
+
+if [ $kaex = 22 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $kaex = 23 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hyammer.py
+fi
+
+if [ $kaex = 24 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+cd TOOLSINSTALLERv1
+sh Tuanb4dut.sh
+fi
+
+if [ $kaex = 25 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv2
+cd TOOLSINSTALLERv2
+sh Tuanb4dut.sh
+fi
+
+if [ $kaex = 26 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+sh TUANB4DUT.sh
+fi
+
+if [ $kaex = 27 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+chmod +x TUANB4DUT..sh
+./TUANB4DUT..sh
+fi
+
+if [ $kaex = 28 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/TUANB4DUT/PERKIRAAN-CUACA
+cd PERKIRAAN-CUACA
+sh CUACA.sh
+fi
+
+if [ $kaex = 29 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/blackcodercrush/shortlink
+cd shortlink
+python2 shortlink.py
+fi
+
+if [ $kaex = 30 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/black-ware/Dorking
+cd Dorking
+sh Dork.sh
+fi
+
+if [ $kaex = 31 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/KALSELeXploit/AdminFinder
+cd AdminFinder
+python2 admin.py
+fi
+
+if [ $kaex = 32 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/zsecc0de-crew-id/adminscan
+cd adminscan
+bash admin.sh
+fi
+
+if [ $kaex = 33 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/Xi4u7/A-Rat
+cd A-Rat
+chmod 777 *
+python2 A-Rat.py
+fi
+
+if [ $kaex = 34 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/evait-security/weeman
+cd weeman
+python2 weeman.py
+fi
+
+if [ $kaex = 35 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocilaFish
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $kaex = 36 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+
+if [ $kaex = 37 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+python2 d-tect.py
+fi
+
+if [ $kaex = 38 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/maldevel/IPGeoLocation
+cd IPGeoLocation
+pip install -r requirements.txt
+python ipgeolocation.py
+fi
+
+if [ $kaex = 39 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+cmatrix
+fi
+
+if [ $kaex = 40 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+cd termux-ohmyzsh
+./install.sh
+fi
+
+if [ $kaex = 41 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/TechnicalMujeeb/PassGen
+cd PassGen
+python passgen.py
+fi
+
+if [ $kaex = 42 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/TechnicalMujeeb/LazyFiglet
+cd LazyFiglet
+sh install.sh
+sh lfiglet
+fi
+
+if [ $kaex = 43 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+telnet mapscii.me
+fi
+
+if [ $kaex = 44 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/TechnicalMujeeb/Termux-Lazyscript
+cd Termux-Lazyscript
+sh setup.sh
+fi
+
+if [ $kaex = 45 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+sl
+fi
+
+if [ $kaex = 46 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/Aditya021/SpamMail
+mv SpamMail $HOME
+cd $HOME/SpamMail
+chmod +x SpamMail.php
+php SpamMail.php
+fi
+
+if [ $kaex = 47 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+neofetch
+fi
+
+if [ $kaex = 48 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/InYourG00D1/Unix
+mv Unix $HOME
+cd $HOME/Unix
+chmod +x KD-FB.sh
+sh KD-FB.sh
+fi
+
+if [ $kaex = 49 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK"|lolcat
+sleep 1.8
+git clone https://github.com/sumar-kun/audio
+mv audio $HOME
+cd $HOME/audio
+unzip p5w.zip
+sh p5w.sh
+fi
+
+if [ $kaex = 50 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+git clone https://github.com/marioyhzkiell/bugreport
+mv bugreport $HOME
+cd $HOME/bugreport
+chmod +x bugreport.py
+python bugreport.py
+fi
+
+if [ $kaex = 51 ]                                                               
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+mpsyt
+fi
+
+if [ $kaex = 52 ]                                                               
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+git clone https://github.com/AMVengeance/FB-React.git
+mv FB-React $HOME
+cd $HOME/FB-React
+chmod +x start
+./start
+fi
+
+if [ $kaex = 53 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+git clone https://github.com/Harisgitama/termux-loginv2fx.git
+mv termux-loginv2fx $HOME
+cd $HOME/termux-loginv2fx
+chmod +x setup.py
+python2 setup.py
+cd $HOME
+python2 useradd.py
+fi
+
+if [ $kaex = 54 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+git clone https://github.com/karjok/terkey
+mv terkey $HOME
+cd $HOME/terkey
+chmod 777 terkey.py
+python terkey.py
+fi
+
+if [ $kaex = 55 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+git clone https://github.com/ArmyCyb3rNetwork/Temperature
+mv Temperature $HOME
+cd $HOME/Temperature
+chmod +x comparing.py
+python2 comparing.py
+fi
+
+if [ $kaex = 56 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+git clone https://github.com/rezadkim/dark-room
+mv dark-room $HOME
+cd $HOME/dark-room
+python2 chat.py
+fi
+
+if [ $kaex = 57 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+proot -0 -w ~ $PREFIX/bin/bash
+whoami
+fi
+
+if [ $kaex = 58 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+moon-buggy
+fi
+
+if [ $kaex = 59 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+chmod +x sqlmap.py
+python2 sqlmap.py
+fi
+
+if [ $kaex = 60 ]
+then
+clear
+figlet -f slant "TUNGGU GBLK" |lolcat
+sleep 1.8
+git clone https://github.com/blackvkng/viSQL
+mv viSQL $HOME
+cd $HOME/viSQL
+chmod +x viSQL.py
+python2 viSQL.py -h
+fi
+
+if [ $kaex = 99 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install git
+pkg install moon-buggy
+pkg install sl
+pkg install neofetch
+pkg install python2 && pkg install python
+pkg install proot
+pip2 install lolcat
+pip install mps_youtube
+pip install youtube_dl
+pkg install mpv
+pip2 install requests
+pip2 install mechanize
+pip2 install dirhunt
+pip2 install youtube-dl
+pkg install curl
+pkg install php
+pip2 install termcolor
+pip2 install bs4
+pip2 install beautifulsoup
+pip2 install colorama
+pkg install perl
+pkg install ruby
+pip install requests
+pkg install figlet
+fi
+
+if [ $kaex = 00 ]
+then
+clear
+echo "See You GoodBye :*..." |lolcat
+exit
+fi
